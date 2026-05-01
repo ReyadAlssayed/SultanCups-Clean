782f753e6f4611bfbb9033a99b316d2f041dc2d4
Reyad <reyad0669@gmail.com>
2026-05-02 00:21:55 +0200

    Update Add order (Validation , type of payment)

---
+++ SultanCups/Models/PaymentInput.cs
@@ -0,0 +1,9 @@
+﻿namespace SultanCups.Models
+{
+    public class PaymentInput
+    {
+        public string method { get; set; } = "cash";
+
+        public decimal amount { get; set; } = 0;
+    }
+}
